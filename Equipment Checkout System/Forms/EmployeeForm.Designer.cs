f1c41eb76da5a5958d56ac594fb6e09082613107
GameWizzard2 <98629019+GameWizzard2@users.noreply.github.com>
2025-06-06 21:46:53 -0400

    EmployeeForm updated

--- Equipment Checkout System/Forms/EmployeeForm.Designer.cs
+++ Equipment Checkout System/Forms/EmployeeForm.Designer.cs
@@ -30,11 +30,16 @@
         {
             buttonCheckout = new Button();
             buttonCheckin = new Button();
+            labelUser = new Label();
+            statusStripUser = new StatusStrip();
+            label2 = new Label();
+            labelCheckedOutTools = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
             // buttonCheckout
             // 
-            buttonCheckout.Location = new Point(128, 100);
+            buttonCheckout.Location = new Point(368, 296);
             buttonCheckout.Name = "buttonCheckout";
             buttonCheckout.Size = new Size(112, 34);
             buttonCheckout.TabIndex = 0;
@@ -44,28 +49,88 @@
             // 
             // buttonCheckin
             // 
-            buttonCheckin.Location = new Point(128, 140);
+            buttonCheckin.Location = new Point(111, 274);
             buttonCheckin.Name = "buttonCheckin";
             buttonCheckin.Size = new Size(112, 34);
             buttonCheckin.TabIndex = 1;
             buttonCheckin.Text = "Checkin";
             buttonCheckin.UseVisualStyleBackColor = true;
             // 
+            // labelUser
+            // 
+            labelUser.AutoSize = true;
+            labelUser.Location = new Point(129, 77);
+            labelUser.Name = "labelUser";
+            labelUser.Size = new Size(59, 25);
+            labelUser.TabIndex = 2;
+            labelUser.Text = "label1";
+            labelUser.Click += labelUser_Click;
+            // 
+            // statusStripUser
+            // 
+            statusStripUser.ImageScalingSize = new Size(24, 24);
+            statusStripUser.Location = new Point(0, 428);
+            statusStripUser.Name = "statusStripUser";
+            statusStripUser.Size = new Size(800, 22);
+            statusStripUser.TabIndex = 3;
+            statusStripUser.Text = "statusStrip1";
+            statusStripUser.ItemClicked += statusStripUser_ItemClicked;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(462, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(227, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Current Tools Checked Out:";
+            label2.Click += label1_Click;
+            // 
+            // labelCheckedOutTools
+            // 
+            labelCheckedOutTools.AutoSize = true;
+            labelCheckedOutTools.Location = new Point(535, 77);
+            labelCheckedOutTools.Name = "labelCheckedOutTools";
+            labelCheckedOutTools.Size = new Size(59, 25);
+            labelCheckedOutTools.TabIndex = 5;
+            labelCheckedOutTools.Text = "label1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(129, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 25);
+            label1.TabIndex = 6;
+            label1.Text = "User:";
+            // 
             // EmployeeForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(labelCheckedOutTools);
+            Controls.Add(label2);
+            Controls.Add(statusStripUser);
+            Controls.Add(labelUser);
             Controls.Add(buttonCheckin);
             Controls.Add(buttonCheckout);
             Name = "EmployeeForm";
             Text = "Employee";
+            Load += EmployeeForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button buttonCheckout;
         private Button buttonCheckin;
+        private Label labelUser;
+        private StatusStrip statusStripUser;
+        private Label label2;
+        private Label labelCheckedOutTools;
+        private Label label1;
     }
 }
--- Equipment Checkout System/Forms/EmployeeForm.cs
+++ Equipment Checkout System/Forms/EmployeeForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Equipment_Checkout_System.Services;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +14,34 @@ namespace Equipment_Checkout_System.Forms
 {
     public partial class EmployeeForm : Form
     {
+        private Employee currentUser;
+
         public EmployeeForm()
         {
             InitializeComponent();
+            currentUser = SessionManager.CurrentUser;
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+            labelUser.Text = $"{currentUser.FirstName} {currentUser.LastName}";
+        }
+
+        private void labelUser_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void statusStripUser_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
